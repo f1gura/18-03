06d74562a2739c0ee8421a874ea2b41d6c080150
f1gura <115150852+f1gura@users.noreply.github.com>
2025-03-18 22:12:16 -0300

    Add files via upload

---
+++ ex3_while.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(void) {
+  int a = 0;
+  int b,c,d,e;
+
+  while(a != -1) {
+  	
+  	puts ("(-1 para encerrar)");
+   
+   puts ("insira um valor: ");
+   scanf("%d",&b);
+   
+   puts ("insira um valor: ");
+   scanf("%d",&c);
+   
+   puts ("insira um valor : ");
+   scanf("%d",&d);
+    
+  }
+  
+  	if(b>=c){
+		if(b>=d){
+			puts("b e o maior ");
+		}
+	}
+
+
+  return 0;
+}
