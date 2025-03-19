078e08e11c34ac353f61e7ae0951dbd172d7388c
f1gura <115150852+f1gura@users.noreply.github.com>
2025-03-19 19:54:27 -0300

    Update ex3_while.c

--- ex3_while.c
+++ ex3_while.c
@@ -1,30 +1,51 @@
 #include <stdio.h>
 
-int main(void) {
-  int a = 0;
-  int b,c,d,e;
+int main() {
+	
+	int a, b, c, cm, c1, c2;
 
-  while(a != -1) {
-  	
-  	puts ("(-1 para encerrar)");
-   
-   puts ("insira um valor: ");
-   scanf("%d",&b);
-   
-   puts ("insira um valor: ");
-   scanf("%d",&c);
-   
-   puts ("insira um valor : ");
-   scanf("%d",&d);
+    printf("Digite o 1 numero: ");
+	scanf("%d", &a);
     
-  }
-  
-  	if(b>=c){
-		if(b>=d){
-			puts("b e o maior ");
-		}
-	}
+	while(a != -1)
+	{
+    printf("Digite o 2 numero: ");
+    scanf("%d", &b);
+    
+    printf("Digite o 3 numero: ");
+    scanf("%d", &c);
 
 
+        if(a > b){
+            if(a > c){
+                
+                cm = a;
+                c1     = b;
+                c2     = c;
+                
+            }
+        }else if(b > c){
+            if(b > a){
+                cm = b;
+                c1 = a;
+                c2 = c;
+            }
+        } else {
+                cm = c;
+                c1     = a;
+                c2     = b;
+        }
+
+
+        if((c1 * c1) + (c2 * c2) == (cm * cm) )
+            puts("Forma um triangulo retangulo.\n");
+        else
+            puts("Não forma um triangulo retangulo.\n");
+        
+	
+	printf("Digite o 1 numero: (-1 para encerrar)");
+	scanf("%d", &a);
+    }
+    
   return 0;
 }
